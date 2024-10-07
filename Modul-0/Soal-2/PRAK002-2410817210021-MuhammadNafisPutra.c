182c5dd402074476bdee23fa19034a8a71357597
MuhammadNafisPutra <muhammadnafisputra@gmail.com>
2024-10-07 12:49:13 +0800

    Add files via upload

---
+++ Modul-0/Soal-2/PRAK002-2410817210021-MuhammadNafisPutra.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Selamat pagi, Muhammad Nafis Putra\n");
+    printf("Selamat siang, Muhammad Nafis Putra\n");
+    printf("Selamat malam, Muhammad Nafis Putra");
+}
