bfcadf2d99d4526cd00286e687af9db6d5752d06
MuhammadNafisPutra <muhammadnafisputra@gmail.com>
2024-10-07 12:21:02 +0800

    Add files via upload

---
+++ Modul-0/Soal-4/PRAK004-2410817210021-MuhammadNafisPutra.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    printf("########n##########################\n");
+    printf("#                                 #\n");
+    printf("#       MUHAMMAD NAFIS PUTRA      #\n");
+    printf("#          2410817210021          #\n");
+    printf("#                                 #\n");
+    printf("########n##########################");
+}
