8668a1c059df90655ebcffc1c72878dacdaf23a2
MuhammadNafisPutra <muhammadnafisputra@gmail.com>
2024-10-07 12:48:56 +0800

    Delete Modul-0/Soal-2/PRAK002-2410817210021-MuhammadNafisPutra.c

--- Modul-0/Soal-2/PRAK002-2410817210021-MuhammadNafisPutra.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("selamat pagi, Muhammad Nafis Putra\n");
-    printf("selamat siang, Muhammad Nafis Putra\n");
-    printf("selamat malam, Muhammad Nafis Putra");
-}
