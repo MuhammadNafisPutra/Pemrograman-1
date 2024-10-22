de82804865aadebe80d2cdaf8b4658a947bde249
MuhammadNafisPutra <muhammadnafisputra@gmail.com>
2024-10-22 12:44:57 +0800

    Add files via upload

---
+++ Modul-1/Soal-9/PRAK109-2410817210021-Muhammad Nafis Putra.py
@@ -0,0 +1,7 @@
+total_pasukan = 958730
+jumlah_pahlawan = 5
+pasukan_per_pahlawan = total_pasukan // jumlah_pahlawan
+
+print("Jumlah pasukan yang dibawa Yu Zhong =", total_pasukan)
+print("Jumlah pahlawan =", jumlah_pahlawan)
+print("Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah", pasukan_per_pahlawan, "pasukan")
