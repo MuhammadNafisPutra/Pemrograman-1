c194ef882a44250be3a4b22eff5afdc1cd15c264
MuhammadNafisPutra <muhammadnafisputra@gmail.com>
2024-10-22 12:25:49 +0800

    Add files via upload

---
+++ Modul-1/Soal-2/PRAK102-2410817210021-MuhammadNafisPutra.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    float a, b, c;
+    a = 4;
+    b = 8;
+    c = 3;
+    printf("Variabel a bernilai %f\n", a);
+    printf("Variabel b bernilai %f\n", b);
+    printf("Variabel c bernilai %f\n", c);
+
+    printf("Hasil dar a dikali b diabgi c adalah %2f", a * b / c);
+    return 0;
+}
