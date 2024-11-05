052cc2da0373c3635af8d4ed59a201b8e5855753
MuhammadNafisPutra <muhammadnafisputra@gmail.com>
2024-11-05 21:43:15 +0800

    Add files via upload

--- Modul-2/Soal-2/.c
+++ Modul-2/Soal-2/.c
@@ -1,11 +1,38 @@
-#include <stdio.h> 
-int main () { 
-int x = 5; 
-int y = 7; 
-int z = 9; 
-printf("Variabel x bernilai %d\n", x); 
-printf("Variabel y bernilai %d\n", y); 
-printf("Variabel z bernilai %d\n", z); 
-printf("Jumlah variabel tersebut adalah %d", x + y + z); 
-return 0; 
+#include <stdio.h>
+
+int main () {
+    char nama [100], NIM [100], KP [100], TTL [100], A [100], H [100], NHP [100];
+
+    printf("Nama                      : ");
+    scanf ("%[^\n]", &nama);
+    getchar();
+    printf("NIM                       : ");
+    scanf ("%[^\n]", &NIM);
+    getchar();
+    printf("Kelas Paralel             : ");
+    scanf ("%[^\n]", &KP);
+    getchar();
+    printf("Tempat/Tanggal Lahir      : ");
+    scanf ("%[^\n]", &TTL);
+    getchar();
+    printf("Alamat                    : ");
+    scanf ("%[^\n]", &A);
+    getchar();
+    printf("Hobi                      : ");
+    scanf ("%[^\n]", &H);
+    getchar();
+    printf("No. HP                    : ");
+    scanf ("%[^\n]", &NHP);
+    getchar();
+
+    printf("\n");
+    printf("Nama                     : %s\n", nama);
+    printf("NIM                      : %s\n", NIM);
+    printf("Kelas Paralel            : %s\n", KP);
+    printf("Tempat/Tanggal Lahir     : %s\n", TTL);
+    printf("Alamat                   : %s\n", A);
+    printf("Hobi                     : %s\n", H);
+    printf("No. HP                   : %s\n", NHP);
+    
+    return 0;
 }
