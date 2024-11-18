d9fcfbf5d30bd347599576b4d43c3cdedf08008b
MuhammadNafisPutra <muhammadnafisputra@gmail.com>
2024-11-18 22:21:08 +0800

    Add files via upload

---
+++ Modul-3/Soal-5/PRAK305-2410817210021-MuhammadNafisPutra.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int hari, jam, menit, detik;
+    printf("");
+    scanf("%d", &detik);
+
+    hari = detik / 86400;
+    jam = (detik % 86400) / 3600;   
+    menit = (detik % 3600) / 60;
+    detik = detik % 60;
+ 
+    if (hari == 0) printf("%02d:%02d:%02d", jam, menit, detik);
+    else printf("%d hari %02d:%02d:%02d", hari, jam, menit, detik);
+
+    return 0;
+}
